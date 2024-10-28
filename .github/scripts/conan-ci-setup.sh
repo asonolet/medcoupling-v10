60337531e8764abb80e9b1968a77e5da4a1286b4
SONOLET Aymeric <aymeric.sonolet@cea.fr>
2024-10-28 14:44:59 +0100

    conan-ci: no need to install for debug config

--- .github/scripts/conan-ci-setup.sh
+++ .github/scripts/conan-ci-setup.sh
@@ -26,6 +26,5 @@ git clone https://github.com/asonolet/salome-conan-packages.git
 conan remote add salome-conan-packages ./salome-conan-packages
 
 conan remove \* --lru=1M -c
-conan install . -b missing -s build_type=Release -o "*:shared=True"
-conan install . -b missing -s build_type=Debug -o "*:shared=True"
+conan install . -b missing -o "*:shared=True"
 conan cache save '*/*:*' --file=conan_cache_save.tgz
