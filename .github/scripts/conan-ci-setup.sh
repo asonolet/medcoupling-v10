499cf4519e8377c339f2421a6f32712f65c7874a
asonolet <aymeric.sonolet@gmail.com>
2025-04-09 08:23:35 +0200

    wip: rm std=17 in conan profile

--- .github/scripts/conan-ci-setup.sh
+++ .github/scripts/conan-ci-setup.sh
@@ -5,18 +5,20 @@ PS4='\033[1;34m>>>\033[0m '
 set -xeu
 
 pip3 install conan
+
 pip3 install "numpy<2"
 pip3 install scipy
+
 pip3 install cmake
 
 conan profile detect -f
 
-std=17
-profile="$(conan profile path default)"
+# std=17
+# profile="$(conan profile path default)"
 
-mv "$profile" "${profile}.bak"
-sed 's/^\(compiler\.cppstd=\).\{1,\}$/\1'"$std/" "${profile}.bak" > "$profile"
-rm "${profile}.bak"
+# mv "$profile" "${profile}.bak"
+# sed 's/^\(compiler\.cppstd=\).\{1,\}$/\1'"$std/" "${profile}.bak" > "$profile"
+# rm "${profile}.bak"
 
 if [ -f conan_cache_save.tgz ]; then
   conan cache restore conan_cache_save.tgz
